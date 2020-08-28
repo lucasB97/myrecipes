cc00b1a8e0a40280afd0766eba8b97d070e4fee0
Lucas Bessa <lucasbessa708@gmail.com>
2020-08-28 15:51:25 -0300

    add new recipe creation with tests

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,18 @@ Rails.application.routes.draw do
   root "pages#home"
   get 'pages/home', to: 'pages#home'
 
+
+#resources :recipes show all the routes to recipe
+resources :recipes
+
+
+
+=begin
   get '/recipes', to: 'recipes#index'
+  get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
+  get '/recipes/:id', to: 'recipes#show', as: 'recipe'
+  post '/recipe', to: 'recipe#create'
+=end
+  
+  
 end
